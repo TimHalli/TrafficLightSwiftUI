0620968bce55a04e1f5ad2c4c122cc072970a663
Tim Halli <t.timhalli@gmail.com>
2023-12-12 17:49:09 +0200

    Change text button & Add Border white

--- TrafficLight/ContentView.swift
+++ TrafficLight/ContentView.swift
@@ -4,6 +4,7 @@ struct ContentView: View {
     private let width: CGFloat = 200
     
     @State private var lightState = 0
+    @State private var buttonText = "START"
     
     var body: some View {
         VStack {
@@ -11,20 +12,24 @@ struct ContentView: View {
             Circle()
                 .frame(width: width)
                 .foregroundColor(.green).opacity(lightState == 0 ? 1: 0.5)
+                .overlay(Circle().stroke(Color.white, lineWidth: 2))
                 .padding()
             
             Circle()
                 .frame(width: width)
                 .foregroundColor(.yellow).opacity(lightState == 1 ? 1: 0.5)
+                .overlay(Circle().stroke(Color.white, lineWidth: 2))
                 .padding()
             
             Circle()
                 .frame(width: width)
                 .foregroundColor(.red).opacity(lightState == 0 ? 1: 0.5)
+                .overlay(Circle().stroke(Color.white, lineWidth: 2))
                 .padding()
             
-            Button("Change Light") {
+            Button(buttonText) {
                 lightState = (lightState + 1) % 3
+                buttonText = lightState == 0 ? "START" : "NEXT"
             }
             .font(.title)
             .padding()
